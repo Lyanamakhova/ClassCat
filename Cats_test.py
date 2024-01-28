1559e01db7ff2dfc577347ea8c318be1a8ae5549
Lyanamakhova <137434562+Lyanamakhova@users.noreply.github.com>
2024-01-28 17:23:47 +0300

    Create Cats_test.py

---
+++ Cats_test.py
@@ -0,0 +1,6 @@
+from ClassCat import Cat
+
+cat1 = Cat("Барон", "мальчик", "2 года")
+cat2 = Cat("Сэм", "мальчик", "2 года")
+print(" Имеющиеся коты на сайте: ", "\n", cat1.getName(), ",", cat1.getGender(), ",", cat1.getAge(), ";")
+print("\n", cat2.getName(), ",", cat2.getGender(), ",", cat2.getAge(), '.')
